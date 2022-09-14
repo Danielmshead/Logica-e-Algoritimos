0028dab95c0cf7ff29ac8ac229eb7b55ae73f511
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-14 10:01:49 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosLogica/Variaveis.cs
@@ -0,0 +1,27 @@
+﻿namespace PraticandoLogicaEx1
+{
+    class Variaveis
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 10;
+            b = 20;
+
+            int bAux = b;
+            b = a;
+            a = bAux;
+
+
+            Console.WriteLine("Resultado a = " + a);
+            Console.WriteLine("Resultado b = " + b);
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
