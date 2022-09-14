886ee09cfb1c808db8d6bf44a480e845fbf0123b
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-14 17:38:37 -0300

    add -  Ex 9

---
+++ Exercicio 9/Ex9.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio9
+{
+    class Ex9
+    {
+        static void Main(string[] args)
+        {
+            int salarioAtual, perReajuste, novoSalario ;
+
+            Console.WriteLine("Digite o salario atual: ");
+            salarioAtual = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o rejuste: ");
+            perReajuste = Convert.ToInt32(Console.ReadLine());
+
+            novoSalario = (salarioAtual * perReajuste / 100);
+            Console.WriteLine("O novo salário é: " + novoSalario + salarioAtual + " %" );
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
