92eade199fef01f7871f0578c3f9e5d790afbf39
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-15 10:51:48 -0300

    Exercicio 10

---
+++ Exercicio 10/Ex10.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio10
+{
+    class Ex10
+    {
+        static void Main(string[] args)
+        {
+            int percDistribuidor = 28;
+            int percImposto = 45;
+
+            Console.WriteLine("Digite o preço de custo da fabrica: ");
+            double valorCustoFabrica = Convert.ToDouble(Console.ReadLine());
+
+            double custoPercDistribuidor = (valorCustoFabrica * percDistribuidor) / 100;
+            double custoPercImposto = (valorCustoFabrica * percImposto) / 100;
+            double valorTotalConsumidor = valorCustoFabrica + custoPercDistribuidor + custoPercImposto;
+
+            Console.WriteLine("O valor do consumidor  é: R$ " + valorTotalConsumidor.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
