2d263a239782b25a311b2ae60e6f37e2239f21c4
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-15 15:48:02 -0300

    Exercicio 18

---
+++ Exercicio 18/Ex18.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercicio18
+{
+    //Ler o ano atual e o ano de nascimento de uma pessoa.Escrever uma mensagem que diga se ela
+    //poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu). 
+    class Ex18
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o ano atual: ");
+            int anoAtual = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano nascimento: ");
+            int anoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            int idade = anoAtual - anoNascimento;
+            if(idade >= 18)
+            {
+                Console.WriteLine("A pessoa tem " + idade + " anos de idade");
+                Console.WriteLine("Pode votar!");
+            }
+            else
+            {
+                Console.WriteLine("A pessoa tem " + idade + " anos de idade");
+                Console.WriteLine("Não pode votar!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
