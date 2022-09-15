e2e11d8cc7fb313a97df17d51266ee59bc56f90f
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-15 16:09:39 -0300

    Exercicio - 20

---
+++ Exercicio 20/Ex20.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercicio20
+{
+    //Ler dois valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.  
+    class Ex20
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            if (valor1 == valor2)
+            {
+                Console.WriteLine("Valores iguais");
+            }
+            else if (valor1 > valor2)
+            {
+                Console.WriteLine(valor2);
+                Console.WriteLine(valor1);
+            }
+            else
+            {
+                Console.WriteLine(valor1);
+                Console.WriteLine(valor2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
