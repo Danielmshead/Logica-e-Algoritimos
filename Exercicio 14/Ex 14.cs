d4d6ddad9621a74616b0b749a6ac15fff6ea50e2
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-15 14:21:01 -0300

    Exercicio 14

---
+++ Exercicio 14/Ex 14.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio14
+{
+    //Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso 
+    //contrário escrever NÃO É MAIOR QUE 10! 
+    class Ex14
+    {
+        static void Main(string[] args)
+        {
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+
+            if (valor > 10)
+            {
+                Console.WriteLine("É MAIOR QUE 10! ");
+            }
+            else
+            {
+                Console.WriteLine(" NÃO É MAIOR QUE 10!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
