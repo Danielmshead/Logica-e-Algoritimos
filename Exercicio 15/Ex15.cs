b6c5e892bec70fae3a7957e64f9c12d231eb7ce4
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-15 14:44:08 -0300

    Exercicio 15

---
+++ Exercicio 15/Ex15.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio15
+{
+    //Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+    class Ex15
+    {
+        static void Main(string[] args)
+        {
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+
+            if (valor >= 0)
+            {
+                Console.WriteLine("Número é positivo");
+            }
+            else
+            {
+                Console.WriteLine("Número é negativo");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
