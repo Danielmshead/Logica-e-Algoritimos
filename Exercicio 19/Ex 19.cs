d006ceac609d0495d1dfa0692ef00170919f94c8
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-09-15 16:08:29 -0300

    Exercicio 19

---
+++ Exercicio 19/Ex 19.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercicio19
+{
+    //Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. 
+    class Ex19
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            if (valor1 == valor2)
+            {
+                Console.WriteLine("Valores são iguais");
+            }
+            if (valor1 > valor2)
+            {
+                Console.WriteLine("O maior valor é: " + valor1);
+            }
+            else if (valor2 > valor1)
+            {
+                Console.WriteLine("O maior valor é: " + valor2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
